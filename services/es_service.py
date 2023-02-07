6ac0c790fe282e0ae871a559fd3b2b333c83a0ae
ozancelikel <72857101+ozancelikel@users.noreply.github.com>
2023-02-07 21:17:35 +0300

    user add updated, get post added aiohttp removed

--- services/es_service.py
+++ services/es_service.py
@@ -7,32 +7,18 @@ class ElasticSearchService:
         self.port = port
         self.index = index
         self.es = Elasticsearch([f"http://{self.ip}:{self.port}"])
-        self.client = aiohttp.ClientSession()
     
     def get_es_result(self, query, filter=None):
-        es_query = self.create_es_query(query, filter)
-        # RUN send_es_request(es_query)
-        return f"ES query result : {query}"
+        es_query = self.create_list_es_query(query, filter)
+        
+        return self.send_es_request(es_query)
+
+    def add_new_entry(self, data):
+        self.es.index(index='ads', doc_type="doc", body=data)
+        return "OK"
 
     def send_es_request(self, query):
-        resp = self.client.post(
-            headers=f"http://{self.ip}:{self.port}/{self.index}",
-            data=query
-        )
-
-        return resp.data
-    
-    def add_new_entry(self, data):
-        self.es.index(index=self.index, body=data)
-
-    @staticmethod
-    def create_add_query(data):
-        new_entry = {
-            "name" : data.get("name"),
-            "phone_number": data.get("phone"),
-            "adress": data.get("adress")
-        }
-        return "query for adding new entry"
+        return self.es.post(query=query)
     
     @staticmethod
     def create_list_es_query(data, filter):
