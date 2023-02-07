c773065744052102555b583d84bdefd89e9c30bf
theFellandes <57584110+theFellandes@users.noreply.github.com>
2023-02-07 22:56:08 +0300

    Add filter and fix get

--- services/es_service.py
+++ services/es_service.py
@@ -1,5 +1,6 @@
 from elasticsearch import Elasticsearch
 
+
 class ElasticSearchService:
     def __init__(self, ip, port, index) -> None:
         self.ip = ip
@@ -7,31 +8,36 @@ class ElasticSearchService:
         self.index = index
         self.es = Elasticsearch([f"http://{self.ip}:{self.port}"])
     
-    def get_es_result(self, query, filter=None):
-        es_query = self.create_list_es_query(query, filter)
+    def get_es_result(self, query):
+        es_query = self.create_list_es_query(query)
         
         return self.send_es_request(es_query)
 
     def add_new_entry(self, data):
-        self.es.index(index=self.index, doc_type="doc", body=data)
+        self.es.index(index=self.index, doc_type="_doc", body=data)
         return "OK"
 
     def send_es_request(self, query):
-        return self.es.indices.get(index="*")
+        return self.es.search(index=self.index, body=query)
 
-    def create_list_es_query(self, data, filter):
-        if filter:
-            body = {
-                    "query": {
-                            "match_all": {}
-                        }
+    def create_list_es_query(self, filter_data):
+        if filter_data:
+            self.es.search(index=self.index, body={
+                "query": {
+                    "match": {
+                        "title": f'{filter_data}'
                     }
-            return body
-        return "unfiltered list query"
+                }
+            })
+        return {
+            "query": {
+                "match_all": {}
+            }
+        }
 
     @staticmethod
     def scroll(self, index: str, body, scroll: str, size: int, **kwargs):
-        page = self.es.search(index=index, body=body, scroll=scroll, size=size, **kwargs)
+        page = self.es.search(index=index, doc_type="_doc", body=body, scroll=scroll, size=size, **kwargs)
         scroll_id = page['_scroll_id']
         hits = page['hits']['hits']
         while len(hits):
@@ -39,6 +45,4 @@ class ElasticSearchService:
             page = self.es.scroll(scroll_id=scroll_id, scroll=scroll)
             scroll_id = page['_scroll_id']
             hits = page['hits']['hits']
-
-
 
